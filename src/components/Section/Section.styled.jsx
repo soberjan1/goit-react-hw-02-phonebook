26051f56a688f973259c0b9e63adf5736fd48275
soberjan1 <aliguseynov48@gmail.com>
2023-07-10 20:37:47 +0300

    title

---
+++ src/components/Section/Section.styled.jsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+export const SectionDiv = styled.div`
+  padding: 10 px 0px;
+`;
+
+export const Title = styled.h1`
+  font-size: 40px;
+  font-weight: 500;
+`;
